917173409af6194550ad104b84d4dd27b1c66fa9
Aden Kejawen <surya.kejawen@gmail.com>
2019-05-18 12:42:12 +0700

    improve: small change to consist with others

--- lib/counter/counter_page.dart
+++ lib/counter/counter_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'counter_bloc.dart';
+import 'package:ad3n/counter/counter_bloc.dart';
 import 'package:ad3n/app.dart';
 
 class CounterPage extends StatelessWidget {
